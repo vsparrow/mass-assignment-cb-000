67f585c52d8ce78ec10a73dcd9675bfc77cc7686
vsparrow <khan@vsparrow.com>
2018-03-02 11:40:54 -0500

    completed practice

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,16 @@
 class Person
-  #your code here
+  # Create a Person class that accepts a hash upon initialization.
+  # The keys of the hash should conform to the attributes below:
+  # allowable properties:
+
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
- # Your initialize method should use iteration and the .send method to mass assign the value of each key/value pair
- # to its associated key (i.e. method). Refer to the previous reading to help you solve this one.
+  # make an attr_accessor for each of the above properties
+
   def initialize(attributes)
-    attributes.each { |key, value| self.send("#{key}=",value)}
-  end #initialize
-
-
+    attributes.each { |property,value| self.send("#{property}=",value)}
+  end
+  # Your initialize method should use iteration and the .send method to mass assign the value of each key/value pair
+  # to its associated key (i.e. method).
 end
